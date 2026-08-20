195a5f643ce0f16dc474d716ffec7edbb7875731
marireinosos <reinososanchezmariana@gmail.com>
2026-08-20 13:24:10 -0500

    Fix image filename case in app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title ("Mi primera App!!")
 
 st.header ("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write ("facilmete puedo realiza beckend y frontend.")
-image = Image.open('Interfaces LanaDelRey.jpg')
+image = Image.open('Interfaces lanaDelRey.jpg')
 st.image (image, caption = 'Interfaces multimodales')
