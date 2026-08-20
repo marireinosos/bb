ef30f06fb4a84591762a78037a9066e6072a412d
marireinosos <reinososanchezmariana@gmail.com>
2026-08-20 13:22:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Hola !!! Mari Reinoso")
+st.title ("Mi primera App!!)
+
+st.header ("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write ("facilmete puedo realiza beckend y frontend.")
+iamge = image.open('Interfaces LanaDelRey.jpg')
+st.image (image, caption = 'Interfaces multimodales')
