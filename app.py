0e573226bfd049bc61bbad7f484b36063a5b1227
marireinosos <reinososanchezmariana@gmail.com>
2026-08-20 13:23:24 -0500

    Fix formatting of title in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title ("Mi primera App!!)
+st.title ("Mi primera App!!")
 
 st.header ("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write ("facilmete puedo realiza beckend y frontend.")
